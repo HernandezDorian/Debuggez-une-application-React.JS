32a14154aa19f575b821c9a36f9d7f4ffc1329df
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2024-02-05 14:36:07 +0100

    Debug filtre

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -14,18 +14,18 @@ const EventList = () => {
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+    (!type ? data?.events : data?.events.filter((evt) => evt.type === type)) ||
+    []
+  ) // On filtre les events en fonction du type
+    .filter((event, index) => {
+      if (
+        (currentPage - 1) * PER_PAGE <= index &&
+        PER_PAGE * currentPage > index
+      ) {
+        return true;
+      }
+      return false;
+    });
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
